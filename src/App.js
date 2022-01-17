7642320213747cdcfddddb0546aaa5a3d97a921e
Tasha <trn.tasha@gmail.com>
2022-01-16 18:27:38 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      Weather App
-      <footer>
-        This is created by{" "}
-        <a
-          href="https://nervous-nightingale-f156b1.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Tasha Tran{" "}
-        </a>{" "}
-        and is open-sourced on {""}
-        <a
-          href="https://github.com/tashatran/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This is created by{" "}
+          <a
+            href="https://nervous-nightingale-f156b1.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tasha Tran{" "}
+          </a>{" "}
+          and is open-sourced on {""}
+          <a
+            href="https://github.com/tashatran/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
